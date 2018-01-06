7513b7e335d0414d0047923a420c343ee9f9aa47
Championz29 <31649202+championz29@users.noreply.github.com>
2018-01-06 20:34:05 +0700

    StackApp.java

---
+++ StackApp.java
@@ -0,0 +1,25 @@
+package Jakkapong;
+
+/**@Jakkapong Buddhachaiyaphum 5907401005
+ */
+
+
+public class StackApp {
+
+	public static void main(String[] args)
+    {
+    StackX theStack = new StackX(10);  // make new stack
+    theStack.push(20);               // push items onto stack
+    theStack.push(40);
+    theStack.push(60);
+    theStack.push(80);
+
+    while( !theStack.isEmpty() )     // until it's empty,
+       {                             // delete item from stack
+       double value = theStack.pop();
+       System.out.print(value);      // display it
+       System.out.print(" ");
+       }  // end while
+    System.out.println("");
+    }  // end main()
+ }  // end class StackApp
